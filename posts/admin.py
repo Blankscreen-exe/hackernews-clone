8dd22d1837742dde0cf79080efcf5fa3811586ec
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-08-17 02:21:09 +0500

    add models

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from posts.models import Post,Comment,Like,PostTag,Tag
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(PostTag)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(Like)
--- posts/models.py
+++ posts/models.py
@@ -5,14 +5,34 @@ from django.contrib.auth.models import User
 from datetime import datetime
 from common.models import TimeStampedModel
 
+class Tag(models.Model):
+    name = models.CharField(_('Tag Name'), max_length=25)
+
+    def __str__(self):
+        return self.name
 class Post(TimeStampedModel):
     title = models.CharField(_("Title"), max_length=50)
     content = models.TextField(_('Content'))
     tags = models.CharField(_('Tags'), max_length=200)
+    tags = models.ManyToManyField(Tag, through='PostTag')
     uploader = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
 
     def get_total_likes(self):
         return Like.objects.filter(post=self).count()
+    
+    @property
+    def get_tags(self):
+        return self.objects.all().prefetch_related('tags')
+    
+    def __str__(self):
+        return f"({self.id}) {self.title}"
+
+class PostTag(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Post: {self.post_id.id} - Tag: {self.tag_id}"
 
 class Comment(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
