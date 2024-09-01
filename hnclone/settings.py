d4a795214f2adb1e535ef705930e474c572416aa
Грабчук Михаил Петрович <mgrabchuk@sv-tech.local>
2024-09-01 14:25:39 +0300

    Postgres involved
    Just involved default postgres setup without any features

--- hnclone/settings.py
+++ hnclone/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'hnclone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hackernews-clonedb',
+        'USER': 'admin',
+        'PASSWORD': 'defpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
